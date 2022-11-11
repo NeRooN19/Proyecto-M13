e2e3c08f41d60c638e6553fc75d4710ad3966398
Pablo <pablo.rico.19@gmail.com>
2022-11-11 23:19:16 +0100

    no message

---
+++ target/generated-sources/annotations/data/Platforms_.java
@@ -0,0 +1,14 @@
+package data;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2022-11-11T23:18:07", comments="EclipseLink-2.7.9.v20210604-rNA")
+@StaticMetamodel(Platforms.class)
+public class Platforms_ { 
+
+    public static volatile SingularAttribute<Platforms, String> name;
+    public static volatile SingularAttribute<Platforms, Integer> ID;
+
+}
---
+++ target/generated-sources/annotations/data/User_.java
@@ -0,0 +1,23 @@
+package data;
+
+import data.GameScore;
+import data.Rental;
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2022-11-11T23:18:07", comments="EclipseLink-2.7.9.v20210604-rNA")
+@StaticMetamodel(User.class)
+public class User_ { 
+
+    public static volatile SingularAttribute<User, String> password;
+    public static volatile SingularAttribute<User, String> mail;
+    public static volatile ListAttribute<User, GameScore> scores;
+    public static volatile SingularAttribute<User, String> name;
+    public static volatile SingularAttribute<User, Boolean> isAdmin;
+    public static volatile SingularAttribute<User, Integer> id;
+    public static volatile SingularAttribute<User, String> username;
+    public static volatile ListAttribute<User, Rental> rental;
+
+}
